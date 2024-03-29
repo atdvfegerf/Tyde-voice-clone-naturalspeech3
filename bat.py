77a52c27690a2f7243c8fc0ef7e73f9d6a3e89a9
atdvfegerf <143095093+atdvfegerf@users.noreply.github.com>
2024-03-29 14:18:15 +0100

    Update bat.py

--- bat.py
+++ bat.py
@@ -1,11 +1,26 @@
+!pip install wget
+
 import wget
-urls = [
-    "https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_decoder.bin?download=true",
-    "https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_decoder_v2.bin?download=true",
-    "https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_encoder.bin?download=true"
-    "https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_encoder_v2.bin?download=true"
-    "https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_redecoder.bin?download=true"
+import os
+
+# Lista di tuple contenenti l'URL del file e il nome desiderato del file
+file_info = [
+    ("https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_decoder.bat?download=true", "ns3_facodec_decoder.bat"),
+    ("https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_decoder_v2.bat?download=true", "ns3_facodec_decoder_v2.bat"),
+    ("https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_encoder.bat?download=true", "ns3_facodec_encoder.bat"),
+    ("https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_encoder_v2.bat?download=true", "ns3_facodec_encoder_v2.bat"),
+    ("https://huggingface.co/spaces/amphion/naturalspeech3_facodec/resolve/main/ns3_facodec_redecoder.bat?download=true", "ns3_facodec_redecoder.bat")
 ]
-directory = "/content/Tyde-voice-clone-naturalspeech3"
-for url in urls:
-    wget.download(url, out=directory)
+
+# Directory di destinazione
+directory = "/content/Tyde-voice-clone-naturalspeech3 "  # Cambia la directory in quella che desideri
+
+# Scarica ciascun file nella directory specificata
+for url, file_name in file_info:
+    # Rinomina il file locale se esiste già
+    local_file_path = os.path.join(directory, file_name)
+    if os.path.exists(local_file_path):
+        base_name, ext = os.path.splitext(file_name)
+        file_name = f"{base_name}_downloaded{ext}"
+    # Scarica il file con il nome desiderato nella directory specificata
+    wget.download(url, out=directory, bar=None, out_filename=file_name)
